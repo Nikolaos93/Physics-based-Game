e14e83dee35bf4662b3c86e38d2af6bf1ac12d13
Nikolaos93 <jelic.nikolaa@gmail.com>
2024-10-04 21:02:31 +0200

    Implemented an arrow vector above the player that corresponds to the velocity of the river
    - Implemented an arrow vector above the player that indicates the velocity of the river (greater in the middle, smaller closer to the bank)
    - This vector is a great contrast to the arrow vector that indicates player's velocity and direction

--- Assets/Scripts/ArrowVectors2.cs
+++ Assets/Scripts/ArrowVectors2.cs
@@ -6,8 +6,11 @@ public class ArrowVectors2 : MonoBehaviour
 {
     public GameObject player;  // Reference to the player object (ball/point/vehicle)
     private PlayerController2 playerController;
-    private Vector3 offset = new Vector3(0, -5, 0); // offset the camera behind the player by adding to the player's position
+    private Vector3 offset1 = new Vector3(0, -5, 0); // offset the camera behind the player by adding to the player's position
+    private Vector3 offset2 = new Vector3(0.25f, -5, 0); // offset the camera behind the player by adding to the player's position
     //private Vector3 offsetFront = new Vector3(0, 2.25f, 1); //offset the camera (1st person)
+    public GameObject arrowVector1;
+    public GameObject arrowVector2;
 
     private Vector3 vectorMagnitude;
 
@@ -16,13 +19,18 @@ public class ArrowVectors2 : MonoBehaviour
     {
         playerController = GameObject.Find("Player").GetComponent<PlayerController2>();
         vectorMagnitude = transform.localScale;
+        arrowVector1 = GameObject.Find("Arrow 1");
+        arrowVector2 = GameObject.Find("Arrow 2");
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + offset;
-        transform.rotation = player.transform.rotation;
-        transform.localScale = vectorMagnitude + new Vector3(0, 0, vectorMagnitude.z + (vectorMagnitude.z * playerController.speed * 0.4f));
+        arrowVector1.transform.position = player.transform.position + offset1;
+        arrowVector1.transform.rotation = player.transform.rotation;
+        arrowVector1.transform.localScale = vectorMagnitude + new Vector3(0, 0, vectorMagnitude.z + (vectorMagnitude.z * playerController.speed * 0.4f));
+
+        arrowVector2.transform.position = player.transform.position + offset2;
+        arrowVector2.transform.localScale = vectorMagnitude + new Vector3(0, 0, vectorMagnitude.z + (vectorMagnitude.z * playerController.speedOfRiver * 0.4f));
     }
 }
