21965e322f5f3e0d3a806039ea9ba6f78e28dd65
Nikolaos93 <jelic.nikolaa@gmail.com>
2024-10-09 01:47:14 +0200

    Refactored and commented code in scripts for Level 3
    - Refactored and commented code in "ArrowVecctors2.cs"
    - Refactored and commented code in "PlayerController2.cs"

--- Assets/Scripts/ArrowVectors2.cs
+++ Assets/Scripts/ArrowVectors2.cs
@@ -5,32 +5,32 @@ using UnityEngine;
 public class ArrowVectors2 : MonoBehaviour
 {
     public GameObject player;  // Reference to the player object (ball/point/vehicle)
-    private PlayerController2 playerController;
-    private Vector3 offset1 = new Vector3(0, -5, 0); // offset the camera behind the player by adding to the player's position
-    private Vector3 offset2 = new Vector3(0.25f, -5, 0); // offset the camera behind the player by adding to the player's position
-    //private Vector3 offsetFront = new Vector3(0, 2.25f, 1); //offset the camera (1st person)
-    public GameObject arrowVector1;
-    public GameObject arrowVector2;
+    private PlayerController2 playerController; // Reference to the PlayerController2 script
+    private Vector3 offset1 = new Vector3(0, -5, 0); // offset the vector behind the player by adding to the player's position
+    private Vector3 offset2 = new Vector3(0.25f, -5, 0); // offset the vector behind&beside the player by adding to the player's position
 
-    private Vector3 vectorMagnitude;
+    public GameObject arrowVector1; // Game object for the vector that represents player's velocity
+    public GameObject arrowVector2; // Game object for the vector that represents velocity of the river
+
+    private Vector3 vectorMagnitude; // Variable that will scale the magnitude of the vector
 
     // Start is called before the first frame update
     void Start()
     {
-        playerController = GameObject.Find("Player").GetComponent<PlayerController2>();
-        vectorMagnitude = transform.localScale;
-        arrowVector1 = GameObject.Find("Arrow 1");
-        arrowVector2 = GameObject.Find("Arrow 2");
+        playerController = GameObject.Find("Player").GetComponent<PlayerController2>(); // Finding Player game object and its component script
+        vectorMagnitude = transform.localScale; // Initital scale/magnitude of the vector arrow
+        arrowVector1 = GameObject.Find("Arrow 1"); // assigning "Arrow 1" game object as "player's vector"
+        arrowVector2 = GameObject.Find("Arrow 2"); // assigning "Arrow 2" game object as "river's vector"
     }
 
     // Update is called once per frame
     void Update()
     {
-        arrowVector1.transform.position = player.transform.position + offset1;
-        arrowVector1.transform.rotation = player.transform.rotation;
-        arrowVector1.transform.localScale = vectorMagnitude + new Vector3(0, 0, vectorMagnitude.z + (vectorMagnitude.z * playerController.speed * 0.4f));
+        arrowVector1.transform.position = player.transform.position + offset1; // Updating the position of the player's vector arrow by adding the offset to the player's position
+        arrowVector1.transform.rotation = player.transform.rotation; // Updating the rotation of the player's vector arrow to match the orientation of the player
+        arrowVector1.transform.localScale = vectorMagnitude + new Vector3(0, 0, vectorMagnitude.z + (vectorMagnitude.z * playerController.speed * 0.4f)); // Scaling the vector by appropriate magnitude (in terms of player's vellocity)
 
-        arrowVector2.transform.position = player.transform.position + offset2;
-        arrowVector2.transform.localScale = vectorMagnitude + new Vector3(0, 0, vectorMagnitude.z + (vectorMagnitude.z * playerController.speedOfRiver * 0.4f));
+        arrowVector2.transform.position = player.transform.position + offset2; // Updating the position of the river's vector arrow by adding the offset to the player's position
+        arrowVector2.transform.localScale = vectorMagnitude + new Vector3(0, 0, vectorMagnitude.z + (vectorMagnitude.z * playerController.speedOfRiver * 0.4f)); // Scaling the vector by appropriate magnitude (in terms of river's vellocity)
     }
 }
--- Assets/Scripts/PlayerController2.cs
+++ Assets/Scripts/PlayerController2.cs
@@ -7,14 +7,14 @@ using UnityEngine;
 public class PlayerController2 : MonoBehaviour
 {
     // Variables for player movement
-    public float speed = 7.0f;
     public Rigidbody playerRb;
-    private float turnSpeed = 25.0f;
+    public float speed = 7.0f; // Initital speed/velocity of the player (ski-jet)
+    private float turnSpeed = 25.0f; // Rotation speed of the player
     private float horizontalInput;
     private float forwardInput;
 
-    public float speedOfRiver = 7.0f;
-    private float speedUpstream;
+    public float speedOfRiver = 7.0f; // Initial speed/velocity of the river (it changes/varies)
+    private float speedUpstream; // Resulting velocity of the player (result of vector addition of player's velocity and river's velocity)
 
     private GameManager gameManager; // This is the GameManager script that checks if the game is active
 
@@ -27,110 +27,97 @@ public class PlayerController2 : MonoBehaviour
     public TextMeshProUGUI distanceText;
     public TextMeshProUGUI speedOfRiverText;
 
-    public ParticleSystem waterSplashParticle;
-    //public AudioClip jetSkiEngineSound;
-    private AudioSource playerAudio;
+    public ParticleSystem waterSplashParticle; // Reference to the particle system (child component of the player)
+    private AudioSource playerAudio; // Reference to the audio source attached to the player
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>(); // Getting the Rigidbody component from the player object
-        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>(); // Finding the GameManager object and get the GameManager script from it
+        playerAudio = GetComponent<AudioSource>(); // Getting the AudioSource component from the player object
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>(); // Finding the GameManager and getting the GameManager script from it
         lastPosition = transform.position; // Set the last position of the player to the starting position
 
-        speedUpstream = speed - speedOfRiver;
-
-        playerAudio = GetComponent<AudioSource>();
+        speedUpstream = speed - speedOfRiver; // Calculating the resulting speed when the game starts (intentionally results to 0 at start)
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*if (gameManager.isGameActive) // If the game is active, then the player can move
-        {
-            float forwardInput = Input.GetAxis("Vertical");
-            float horizontalInput = Input.GetAxis("Horizontal");
-            playerRb.AddForce(Vector3.forward * speed * forwardInput);
-            playerRb.AddForce(Vector3.right * speed * horizontalInput);
-        }*/
 
-        speedUpstream = speed - speedOfRiver;
+        speedUpstream = speed - speedOfRiver; // Continuosly calculating the resulting speed
+
         if (gameManager.isGameActive) // If the game is active, then the player can move
         {
-            horizontalInput = Input.GetAxis("Horizontal");
-            forwardInput = Input.GetAxis("Vertical");
+            horizontalInput = Input.GetAxis("Horizontal"); // Getting horizontal input
+            forwardInput = Input.GetAxis("Vertical"); // Getting vertical input
 
             if (Input.GetKeyDown(KeyCode.Space) && speed < 10)
             {
-                speed++;
+                speed++; // Incrementing speed if Space is pressed and speed is less than 10m/s
             }
             if (Input.GetKeyDown(KeyCode.LeftControl) && speed > 0)
             {
-                speed--;
+                speed--; // Decrementing speed if LCtrl is pressed and speed is greater than 0m/s
             }
 
-            // Move the vehicle forward
-            if (transform.position.x > -1 && transform.position.x < 1)
+            // Move the player forward
+            if (transform.position.x > -1 && transform.position.x < 1) // Center/middle "lane" of the river
             {
-                speedOfRiver = 7;
-                transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput, Space.Self);
-                transform.Translate(-Vector3.forward * Time.deltaTime * speedOfRiver, Space.World);
+                speedOfRiver = 7; // Flow of the river is the fastest here because it is in the center
+                transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput, Space.Self); // Moving the player forward, based on local coordinates (frame of reference)
+                transform.Translate(-Vector3.forward * Time.deltaTime * speedOfRiver, Space.World); // Moving the player in the direction of the river, based on global coordinates (frame of reference)
             }
-            else if (transform.position.x > -3 && transform.position.x < 3)
+            else if (transform.position.x > -3 && transform.position.x < 3) // 1st lanes to the left and to the right of the middle lane
             {
-                speedOfRiver = 5;
-                transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput, Space.Self);
-                transform.Translate(-Vector3.forward * Time.deltaTime * speedOfRiver, Space.World);
-                //playerRb.AddForce(Vector3.forward * Time.deltaTime * speedUpstream * forwardInput);
+                speedOfRiver = 5; // Flow of the river is a bit slower here than in the middle
+                transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput, Space.Self); // Moving the player forward, based on local coordinates (frame of reference)
+                transform.Translate(-Vector3.forward * Time.deltaTime * speedOfRiver, Space.World); // Moving the player in the direction of the river, based on global coordinates (frame of reference)
             }
-            else
+            else // Lanes by the river banks
             {
-                speedOfRiver = 3;
-                transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput, Space.Self);
-                transform.Translate(-Vector3.forward * Time.deltaTime * speedOfRiver, Space.World);
-                //playerRb.AddForce(Vector3.right * Time.deltaTime * speedUpstream * horizontalInput);
+                speedOfRiver = 3; // Flow of the river is the slowest here (next to the river banks)
+                transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput, Space.Self); // Moving the player forward, based on local coordinates (frame of reference)
+                transform.Translate(-Vector3.forward * Time.deltaTime * speedOfRiver, Space.World); // Moving the player in the direction of the river, based on global coordinates (frame of reference)
             }
-            /*{
-                speed = 10;
-                transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-            }*/
-            if (Input.GetKeyDown(KeyCode.W))
+            
+            if (Input.GetKeyDown(KeyCode.W)) // Checking whether the player wants to move forward
             {
-                waterSplashParticle.Play();
-                playerAudio.loop = true;
-                playerAudio.Play();
+                waterSplashParticle.Play(); // Playing the water splashing particle effect
+                playerAudio.loop = true; // Setting looping of the jet-ski sound to true
+                playerAudio.Play(); // Playing the jet-ski sound
             }
-            else if (Input.GetKeyUp(KeyCode.W)) 
+            else if (Input.GetKeyUp(KeyCode.W)) // Checking whether the player wants to stop moving forward
             {
-                waterSplashParticle.Stop();
-                playerAudio.Stop();
-                playerAudio.loop = false;
+                waterSplashParticle.Stop(); // Stoping the water splashing particle effect
+                playerAudio.Stop(); // Stopping the jet-ski sound
+                playerAudio.loop = false; // Setting looping of the jet-ski sound to false
             }
             
 
-            // Move the vehicle right(left)
-            transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime); // Replaces the above line
+            // Move/rotate the player right(left)
+            transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime); // Rotating the player based on turnSpeed variable
 
             float distance = Vector3.Distance(lastPosition, transform.position); // Distance between the last position and the current position
             totalDistance += distance; // Adds the distance to the total distance 
             lastPosition = transform.position; // Updates the last position to the current position
         }
 
-        if (!gameManager.isGameActive)
+        if (!gameManager.isGameActive) // Checking if the game is not active
         {
-            waterSplashParticle.Stop();
-            playerAudio.Stop();
-            playerAudio.loop = false;
+            waterSplashParticle.Stop(); // Stoping the water splashing particle effect
+            playerAudio.Stop(); // Stoping the jet-ski sound if the game is not active
+            playerAudio.loop = false; // Looping of the jet-ski sound set to false
         }
 
         playerValues(); // Calls the playerValues method to display the player's stats
     }
 
-    private void playerValues() // This method will display the player's stats (e.g. velocity, distance, acceleration...) on the screen
+    private void playerValues() // This method will display the player's stats on the screen
     {
-        velocityText.text = "Velocity: " + (int)speed + "[m/s]";
-        distanceText.text = "Distance: " + (int)totalDistance + "[m]";
-        speedOfRiverText.text = "River: " + (int)speedOfRiver + "[m/s]";
+        velocityText.text = "Velocity: " + (int)speed + "[m/s]"; // Displaying player's velocity on the screen
+        distanceText.text = "Distance: " + (int)totalDistance + "[m]"; // Displaying distance the player has covered on the screen 
+        speedOfRiverText.text = "River: " + (int)speedOfRiver + "[m/s]"; // Displaying the speed of the river on the screen
     }
 
     private void OnTriggerEnter(Collider other) // This method will be called when the player collides with another object (e.g. the finish line)
@@ -138,14 +125,9 @@ public class PlayerController2 : MonoBehaviour
         if (other.CompareTag("Finish")) // If the player collides with an object that has the tag "Finish", then the level is complete
         {
             gameManager.LevelComplete(); // Call the LevelComplete method from the GameManager script
-            transform.position = new Vector3(0, 0.5f, 0); // Reset the player's position to the starting position
-            Debug.Log("Level Finished");
+            //transform.position = new Vector3(0, 0.5f, 0); // Reset the player's position to the starting position
+            //Debug.Log("Level Finished");
         }
-
-        /*if (other.CompareTag("Checkpoint")) // If the player collides with an object that has the tag "Finish", then the level is complete
-        { ?????????????????????
-            gameManager.CheckpointReached(); // Call the LevelComplete method from the GameManager script
-            Debug.Log("Checkpoint Reached");
-        }*/
     }
+
 }
