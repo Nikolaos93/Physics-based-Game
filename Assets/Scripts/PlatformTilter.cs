b35ad9bea199d3035ca798232393fcab439b11ef
Nikolaos93 <jelic.nikolaa@gmail.com>
2024-10-09 18:34:01 +0200

    Refactored and commented code in scripts for Level 6
    - Refactored and commented code in "PlayerController6.cs"
    - Refactored and commented code in "PlatformTilter.cs"

--- Assets/Scripts/PlatformTilter.cs
+++ Assets/Scripts/PlatformTilter.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlatformTilter : MonoBehaviour
 {
-    public GameManager gameManager;
+    public GameManager gameManager; // Reference to the GameManager script that checks if the game is active
 
     // Start is called before the first frame update
     void Start()
@@ -15,8 +15,10 @@ public class PlatformTilter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (gameManager.isGameActive)
-        {
+
+        if (gameManager.isGameActive) // If the game is active, then tilt the platform
+        {   
+            // Using PingPong method to tilt the platform on x-axis and z-axis
             transform.localEulerAngles = new Vector3(Mathf.PingPong(Time.time * 40, 30) - 15, 0, Mathf.PingPong(Time.time * 30, 40) - 20);
         }
         
--- Assets/Scripts/PlayerController6.cs
+++ Assets/Scripts/PlayerController6.cs
@@ -7,14 +7,12 @@ using UnityEngine;
 public class PlayerController6 : MonoBehaviour
 {
     // Variables for player movement
-    public float speed = 5.0f;
-    //public float acceleration = 1.0f;
     public Rigidbody playerRb;
-    //private float turnSpeed = 25.0f;
+    public float speed = 5.0f;// Initital speed/velocity of the player
     private float horizontalInput;
     private float forwardInput;
 
-    private GameManager gameManager; // This is the GameManager script that checks if the game is active
+    private GameManager gameManager; // Reference to the GameManager script that checks if the game is active
 
     // Variables for tracking player's distance/displacement
     private Vector3 lastPosition;
@@ -25,17 +23,11 @@ public class PlayerController6 : MonoBehaviour
     public TextMeshProUGUI distanceText;
     public TextMeshProUGUI accelerationText;
 
-    //public GameObject cube; // ???
-    //public int checkpointReached = 0; ??
-    //private float maxSpeed = 35.0f;
+    private GameObject focalPoint; // focal point located at the center of the platform
 
-    private GameObject focalPoint; // ???
-
-    /*public GameObject collectable1;
-    public GameObject collectable2;*/
-    public GameObject[] collectables;
-    public Transform[] spawningPoints;
-    public int collectableCount;
+    public GameObject[] collectables; // Array of collectables (stars and diamonds)
+    public Transform[] spawningPoints; // Array of spawning points for collectables
+    public int collectableCount; // Counter for number of collctables
 
     private Bomb bCollectable; // refference to Bomb.cs where number of collected pickups is tracked
 
@@ -44,63 +36,26 @@ public class PlayerController6 : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>(); // Getting the Rigidbody component from the player object
-        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>(); // Finding the GameManager object and get the GameManager script from it
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>(); // Finding the GameManager and getting the GameManager script from it
         lastPosition = transform.position; // Set the last position of the player to the starting position
 
-        focalPoint = GameObject.Find("Focal Point"); // ???
+        focalPoint = GameObject.Find("Focal Point"); // Finding the Focal Point in the scene
 
-        collectableCount = 0;
+        collectableCount = 0; // Initial number of collectables is 0
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*if (gameManager.isGameActive) // If the game is active, then the player can move
-        {
-            float forwardInput = Input.GetAxis("Vertical");
-            float horizontalInput = Input.GetAxis("Horizontal");
-            playerRb.AddForce(Vector3.forward * speed * forwardInput);
-            playerRb.AddForce(Vector3.right * speed * horizontalInput);
-        }*/
 
         if (gameManager.isGameActive) // If the game is active, then the player can move
         {
-            forwardInput = Input.GetAxis("Vertical");
-            horizontalInput = Input.GetAxis("Horizontal");
+            forwardInput = Input.GetAxis("Vertical"); // Getting horizontal input
+            horizontalInput = Input.GetAxis("Horizontal"); // Getting vertical input
 
-            playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
-            playerRb.AddForce(focalPoint.transform.right * speed * horizontalInput);
-
-
-            //Debug.Log(bCollectable.collectedNumber);
-            //Debug.Log(bCollectable.collectedNumber);
-            /*if (collectableCount < bCollectable.collectedNumber)
-            {
-                GameObject collectable = Instantiate(collectables[UnityEngine.Random.Range(0, collectables.Length)], spawningPoints[UnityEngine.Random.Range(0, spawningPoints.Length)]);
-                collectableCount++;
-            }*/
-
-
-            /*if (Input.GetKeyDown(KeyCode.Space) && acceleration < 5)
-            {
-                acceleration++;
-            }
-            if (Input.GetKeyDown(KeyCode.LeftControl) && acceleration > -5)
-            {
-                acceleration--;
-            }
-
-            // Move the vehicle forward
-            if (speed < maxSpeed)
-            {
-                speed += acceleration * Time.deltaTime;
-            }
-            //transform.Translate(Vector3.forward * Time.deltaTime * speed * acceleration * forwardInput);
-            transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-            //transform.position.x = transform.position.x + speed * Time.deltaTime;
-
-            // Move the vehicle right(left)
-            transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime); // Replaces the above line*/
+            // Moving the player 
+            playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput); // Adding force to the player's rigid body component based on vertical input
+            playerRb.AddForce(focalPoint.transform.right * speed * horizontalInput); // Adding force to the player's rigid body component based on horizontal input
 
             float distance = Vector3.Distance(lastPosition, transform.position); // Distance between the last position and the current position
             totalDistance += distance; // Adds the distance to the total distance 
@@ -109,9 +64,8 @@ public class PlayerController6 : MonoBehaviour
 
         playerValues(); // Calls the playerValues method to display the player's stats
 
-        //StartCoroutine(CollectableCountdownRoutine());
 
-        if (transform.position.y < -5)
+        if (transform.position.y < -5) //Checking whether the player has fallen of the platform
         {
             gameManager.GameOver(); // If the player falls off the platform, then the game is over
         }
@@ -119,10 +73,10 @@ public class PlayerController6 : MonoBehaviour
     }
 
     private void playerValues() // This method will display the player's stats (e.g. velocity, distance, acceleration...) on the screen
-    {
-        velocityText.text = "Velocity: " + (int)speed + "[m/s]";
-        distanceText.text = "Distance: " + (int)totalDistance + "[m]";
-        accelerationText.text = "Acceler: " + (int)2 + "[m/s^2]";
+    { 
+        velocityText.text = "Velocity: " + (int)speed + "[m/s]"; // Displaying player's velocity on the screen
+        distanceText.text = "Distance: " + (int)totalDistance + "[m]"; // Displaying distance the player has covered on the screen 
+        accelerationText.text = "Acceler: " + (int)2 + "[m/s^2]"; // Displaying player's acceleration on screen (important to show that there is no acceleration during uniform motion)
     }
 
     private void OnTriggerEnter(Collider other) // This method will be called when the player collides with another object (e.g. the finish line)
@@ -131,10 +85,10 @@ public class PlayerController6 : MonoBehaviour
         {
             gameManager.LevelComplete(); // Call the LevelComplete method from the GameManager script
             //transform.position = new Vector3(0, 0.5f, 0); // Reset the player's position to the starting position
-            Debug.Log("Level Finished");
+            //Debug.Log("Level Finished");
         }
 
-        if (other.CompareTag("Checkpoint")) // If the player collides with an object that has the tag "Finish", then the level is complete
+        if (other.CompareTag("Checkpoint")) // If the player collides with an object that has the tag "Checkpoint", then the checkpoint has been reached
         {
             //checkpointReached++;
             gameManager.CheckpointReached(); // Call the LevelComplete method from the GameManager script  
@@ -142,28 +96,17 @@ public class PlayerController6 : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision) //Checking whether the player has collided with something (collectable on this level)
     {
-        if (collision.gameObject.CompareTag("Gem") && gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Gem") && gameObject.CompareTag("Player")) // Checking if the player has collided with the gem/diamond
         {
-            GameObject collectable = Instantiate(collectables[UnityEngine.Random.Range(0, collectables.Length)], spawningPoints[UnityEngine.Random.Range(0, spawningPoints.Length)]);
+            GameObject collectable = Instantiate(collectables[UnityEngine.Random.Range(0, collectables.Length)], spawningPoints[UnityEngine.Random.Range(0, spawningPoints.Length)]); // if it is spawn a new random collectable at a random spawning point
         }
-        if (collision.gameObject.CompareTag("Star") && gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Star") && gameObject.CompareTag("Player")) // Checking if the player has collided with the star
         {
-            GameObject collectable = Instantiate(collectables[UnityEngine.Random.Range(0, collectables.Length)], spawningPoints[UnityEngine.Random.Range(0, spawningPoints.Length)]);
+            GameObject collectable = Instantiate(collectables[UnityEngine.Random.Range(0, collectables.Length)], spawningPoints[UnityEngine.Random.Range(0, spawningPoints.Length)]); // if it is spawn a new random collectable at a random spawning point
         }
 
     }
-
-    /*IEnumerator CollectableCountdownRoutine()
-    {
-        yield return new WaitForSeconds(6);
-        SpawnCollectable();
-    }
-
-    private void SpawnCollectable()
-    {
-        GameObject collectable = Instantiate(collectables[UnityEngine.Random.Range(0, collectables.Length)], spawningPoints[UnityEngine.Random.Range(0, spawningPoints.Length)]);
-    }*/
 
 }
