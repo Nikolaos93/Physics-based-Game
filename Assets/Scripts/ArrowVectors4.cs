2d0ffb1ab85dd84eab2316895139f204ca879295
Nikolaos93 <jelic.nikolaa@gmail.com>
2024-10-12 02:28:10 +0200

    Fixed arrow vector on Level 1 and Level 4 to proportionally scale in terms of vellocity/acceleration

--- Assets/Scripts/ArrowVectors4.cs
+++ Assets/Scripts/ArrowVectors4.cs
@@ -9,6 +9,7 @@ public class ArrowVectors4 : MonoBehaviour
     private Vector3 offset = new Vector3(0, -5, 0); // offset the vector above the player by adding to the player's position
 
     private Vector3 vectorMagnitude; // Variable that will scale the magnitude of the vector
+    private int arrowDirection = 1; // Variable that will determine the direction of the arrow
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +23,15 @@ public class ArrowVectors4 : MonoBehaviour
     {
         transform.position = player.transform.position + offset; // Updating the position of the vector arrow by adding the offset to the player's position
         transform.rotation = player.transform.rotation; // Updating the rotation of the vector so that it matches the orientation/direction of the player
-        transform.localScale = vectorMagnitude + new Vector3(0, 0, vectorMagnitude.z + (vectorMagnitude.z * playerController4.acceleration * 0.8f)); // Scaling the vector by appropriate magnitude (in terms of acceleration)
+        //transform.localScale = vectorMagnitude + new Vector3(0, 0, vectorMagnitude.z + (vectorMagnitude.z * playerController4.acceleration * 0.8f)); // Scaling the vector by appropriate magnitude (in terms of acceleration)
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            arrowDirection = 1; // If the player is moving forward, the arrow should point in the positive direction
+        }
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            arrowDirection = -1; // If the player is moving backward, the arrow should point in the negative direction
+        }
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, playerController4.acceleration * arrowDirection); // Scaling the vector by appropriate magnitude (in terms of acceleration)
     }
 }
